58d7949dc32d49c7426c4bc8834ae0ed4f70f93e
tommaddoxsoftware <32769550+tommaddoxsoftware@users.noreply.github.com>
2018-03-13 12:37:03 +0000

    Added functionality to UiController
    Now assigns a player a random colour.
    Bug fixed when grabbing text component

--- Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/Ui/UiController.cs
+++ Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/Ui/UiController.cs
@@ -9,9 +9,16 @@ public class UiController : MonoBehaviour {
 
 	public void UISetPlayerName(GameObject playerObj, string usrName)
     {
-        uiText = playerObj.GetComponent<Text>();
+        uiText = playerObj.GetComponentInChildren<Text>();
         uiText.text = usrName;
+        uiText.color = ColorFromUsername(usrName);
     }
 
-   
+    Color ColorFromUsername(string username)
+    {
+        Random.seed = username.Length + (int)username[0] + (int)username[username.Length - 1];
+        return new Color(Random.Range(0.25f, 0.55f), Random.Range(0.20f, 0.55f), Random.Range(0.25f, 0.55f));
+    }
+
+
 }
