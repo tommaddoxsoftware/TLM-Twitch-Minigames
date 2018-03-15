0bf649b0bc96af4b6f44063cfcc777a09c02cd4e
tommaddoxsoftware <32769550+tommaddoxsoftware@users.noreply.github.com>
2018-03-15 12:44:34 +0000

    Extended UI functionality
    UI now adds players to an in-game scoreboard, and includes more comments.

--- Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/Ui/UiController.cs
+++ Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/Ui/UiController.cs
@@ -4,20 +4,52 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UiController : MonoBehaviour {
+
+    private int numPlayers = 0;
  
     private Text uiText;
+    [SerializeField]
+    private GameObject usrNamePrefab;
+    [SerializeField]
+    private GameObject usrScorePrefab;
 
 	public void UISetPlayerName(GameObject playerObj, string usrName)
     {
+        //Get the text object attached to ball UI
         uiText = playerObj.GetComponentInChildren<Text>();
+
+        //Set their name, and set colour to player's assigned colour
         uiText.text = usrName;
-        uiText.color = ColorFromUsername(usrName);
+        uiText.color = playerObj.GetComponent<Ball>().playerColour;
     }
 
-    Color ColorFromUsername(string username)
+    public Color ColorFromUsername(string username)
     {
         Random.seed = username.Length + (int)username[0] + (int)username[username.Length - 1];
         return new Color(Random.Range(0.25f, 0.55f), Random.Range(0.20f, 0.55f), Random.Range(0.25f, 0.55f));
+    }
+
+    public void AddToScoreboard(string username, Color usrColour)
+    {
+        //Add a new piece of UI for the player, one for name, one for score
+        GameObject usrName = Instantiate(usrNamePrefab, GameObject.Find("Scoreboard").transform);
+        GameObject usrScore = Instantiate(usrScorePrefab, GameObject.Find("Scoreboard").transform);
+
+        //Get the text component for each
+        Text name = usrName.GetComponent<Text>();
+        Text score = usrScore.GetComponent<Text>();
+
+        //Set color based on the player's assigned colour
+        name.color = score.color = usrColour;
+
+        //Set the appropriate text.
+        name.text = username;
+        score.text = "0";       
+        
+        //Position the text based on how many players
+        usrName.transform.position = usrName.transform.position - new Vector3(0, 20 * numPlayers, 0);
+        usrScore.transform.position = usrScore.transform.position - new Vector3(0, 20 * numPlayers, 0);
+        numPlayers++;
     }
 
 
