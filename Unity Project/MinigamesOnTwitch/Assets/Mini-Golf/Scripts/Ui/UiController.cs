8e9c58a442def22f0fd0e3663633a766f526784c
tommaddoxsoftware <32769550+tommaddoxsoftware@users.noreply.github.com>
2018-03-15 14:10:14 +0000

    Added functionality to remove from scoreboard and update score
    UI can now be updated when user takes a shot.  Remove feature needs to be updated, currently just changes text to ""Player Left" and sets score to DNF. Will update to resort scoreboard at a later date.

--- Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/Ui/UiController.cs
+++ Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/Ui/UiController.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class UiController : MonoBehaviour {
 
     private int numPlayers = 0;
- 
+
     private Text uiText;
     [SerializeField]
     private GameObject usrNamePrefab;
@@ -25,22 +25,28 @@ public class UiController : MonoBehaviour {
 
     public Color ColorFromUsername(string username)
     {
-        Random.seed = username.Length + (int)username[0] + (int)username[username.Length - 1];
+       Random.seed = username.Length + (int)username[0] + (int)username[username.Length - 1];
         return new Color(Random.Range(0.25f, 0.55f), Random.Range(0.20f, 0.55f), Random.Range(0.25f, 0.55f));
     }
 
-    public void AddToScoreboard(string username, Color usrColour)
+    public void AddToScoreboard(string username, GameObject player)
     {
+        
         //Add a new piece of UI for the player, one for name, one for score
         GameObject usrName = Instantiate(usrNamePrefab, GameObject.Find("Scoreboard").transform);
         GameObject usrScore = Instantiate(usrScorePrefab, GameObject.Find("Scoreboard").transform);
+
+        //Assign the UI GameObjects to ball (Will be used if removing, updating)
+        player.GetComponent<Ball>().scoreboardNameUi = usrName;
+        player.GetComponent<Ball>().scoreBoardStrokeUi = usrScore;
+
 
         //Get the text component for each
         Text name = usrName.GetComponent<Text>();
         Text score = usrScore.GetComponent<Text>();
 
         //Set color based on the player's assigned colour
-        name.color = score.color = usrColour;
+        name.color = score.color = player.GetComponent<Ball>().playerColour;
 
         //Set the appropriate text.
         name.text = username;
@@ -49,7 +55,28 @@ public class UiController : MonoBehaviour {
         //Position the text based on how many players
         usrName.transform.position = usrName.transform.position - new Vector3(0, 20 * numPlayers, 0);
         usrScore.transform.position = usrScore.transform.position - new Vector3(0, 20 * numPlayers, 0);
+        player.GetComponent<Ball>().playerId = numPlayers;
         numPlayers++;
+    }
+
+    public void UpdateScore(Text scoreText, string newScore) {
+        scoreText.text = newScore;
+    }
+
+    public void RemoveFromScoreboard(GameObject player)
+    {
+        int position = player.GetComponent<Ball>().playerId;
+        GameObject name = player.GetComponent<Ball>().scoreboardNameUi;
+        GameObject score = player.GetComponent<Ball>().scoreBoardStrokeUi;
+        name.GetComponent<Text>().text = "PLAYER LEFT";
+        score.GetComponent<Text>().text = "DNF";
+        
+        //This'll be used when I've figured out a way to "resort" the scoreboard
+        /*
+        Destroy(name);
+        Destroy(score);
+        */
+
     }
 
 
