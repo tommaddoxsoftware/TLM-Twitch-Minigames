26e5956c1f0b155261c9b293288449d86634a2f9
tommaddoxsoftware <32769550+tommaddoxsoftware@users.noreply.github.com>
2018-04-12 14:33:52 +0100

    Added UiManager script for various UI tasks
    - Adds twitch auth to playerprefs for future reference
    - Exits Application on button click

---
+++ Unity Project/MinigamesOnTwitch/Assets/Shared_Scripts/UiManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour {
+    [SerializeField]
+    private InputField usernameInput;
+    [SerializeField]
+    private InputField oauthInput;
+
+    private void Start()
+    {
+        //Set listener for input fields. Delegate the function to SetUsername and SetOAuth
+        usernameInput.onEndEdit.AddListener(delegate { SetUsername(usernameInput); });
+        oauthInput.onEndEdit.AddListener(delegate { SetOAuth(oauthInput); });
+    }
+    
+    public void GetTwitchAuth()
+    {
+        Application.OpenURL("https://twitchapps.com/tmi/");
+    }
+
+    private void SetUsername(InputField usr)
+    {
+        PlayerPrefs.SetString("TwitchUsr", usr.text);
+    }
+    
+    private void SetOAuth(InputField auth)
+    {
+        PlayerPrefs.SetString("TwitchAuth", auth.text);
+    }
+
+    public void ExitApplication()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit ();
+        #endif
+    }
+}
