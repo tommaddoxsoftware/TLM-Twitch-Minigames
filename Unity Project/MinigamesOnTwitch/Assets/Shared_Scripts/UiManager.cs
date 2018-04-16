3e36fe6fd3cfff49d8ab2a4dcfe3e60c7953ecbf
tommaddoxsoftware <32769550+tommaddoxsoftware@users.noreply.github.com>
2018-04-16 14:59:48 +0100

    Added Max Player Slider
    Playerprefs now contain MaxPlayers key, sets the maximum number of players that can join when the game starts

--- Unity Project/MinigamesOnTwitch/Assets/Shared_Scripts/UiManager.cs
+++ Unity Project/MinigamesOnTwitch/Assets/Shared_Scripts/UiManager.cs
@@ -9,13 +9,27 @@ public class UiManager : MonoBehaviour {
     [SerializeField]
     private InputField oauthInput;
     [SerializeField]
-    private InputField channelInput;
+    private Slider maxPlayerSlider;
+    [SerializeField]
+    private Text maxPlayerLbl;
 
     private void Start()
     {
         //Set listener for input fields. Delegate the function to SetUsername and SetOAuth
         usernameInput.onEndEdit.AddListener(delegate { SetUsername(usernameInput); });
         oauthInput.onEndEdit.AddListener(delegate { SetOAuth(oauthInput); });
+        maxPlayerSlider.onValueChanged.AddListener(delegate { SetMaxPlayers(maxPlayerSlider); });
+
+        //Set Max Players
+        if (PlayerPrefs.HasKey("MaxPlayers"))
+            maxPlayerLbl.text = PlayerPrefs.GetInt("MaxPlayers").ToString();
+        else
+        {
+            maxPlayerLbl.text = "1";
+            PlayerPrefs.SetInt("MaxPlayers", 1);
+        }
+        
+
 
 
         if (PlayerPrefs.HasKey("TwitchUsr"))
@@ -40,6 +54,12 @@ public class UiManager : MonoBehaviour {
         PlayerPrefs.SetString("TwitchAuth", auth.text);
     }
 
+    private void SetMaxPlayers(Slider maxPlayers)
+    {
+        PlayerPrefs.SetInt("MaxPlayers", (int) maxPlayers.value);
+        maxPlayerLbl.text = maxPlayers.value.ToString();
+
+    }
     public void ExitApplication()
     {
         #if UNITY_EDITOR
