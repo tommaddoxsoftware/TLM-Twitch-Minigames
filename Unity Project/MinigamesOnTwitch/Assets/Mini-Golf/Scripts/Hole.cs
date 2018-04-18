2a0a6fbc8abc831440aed05caca130e6f77ea38c
ElectricPie <32139670+ElectricPie@users.noreply.github.com>
2018-04-18 13:36:59 +0100

    Holes Finds Controller
    Hole now find the level controller instead of being assigned it by a public variable.

--- Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/Hole.cs
+++ Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/Hole.cs
@@ -3,23 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hole : MonoBehaviour {
-    public LevelController controller;
+    private LevelController controller;
 
     // Use this for initialization
     void Start () {
-
+        //Finds the level controller
+        controller = GameObject.Find("MinigameManager").GetComponent<LevelController>();
     }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player") //Checks the other is a ball  ##CHANGE TAG##
+        //Checks the other is a ball  ##CHANGE TAG##
+        if (other.tag == "Player") 
         {
-            controller.BallHole(other.gameObject); //Updates the golf controller
+            //Updates the golf controller
+            controller.BallHole(other.gameObject); 
         }
     }
 }
