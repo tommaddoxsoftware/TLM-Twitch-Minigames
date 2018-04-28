686b794c920094429cffc077d2eeb30bee01891a
ElectricPie <32139670+ElectricPie@users.noreply.github.com>
2018-04-28 14:45:18 +0100

    Created New Bot Reply Script
    Create a new script to handle the responses given by the bot instead of it being in the Ball script. Currently it gets the users name and response with their name.

---
+++ Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/TwitchBot/MinigolfBotReplys.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigolfBotReplys
+{
+    private StringSplitter m_splitter;
+    private TwitchIRC m_irc;
+
+    public MinigolfBotReplys(TwitchIRC irc, GameJoin players)
+    {
+        m_splitter = new StringSplitter();
+        m_irc = irc;
+    }
+
+    public void ProcessCommand(string user, string[] messages)
+    {
+        //Replys the the user
+        string response = user + " ";
+
+        if (messages[0].ToLower() == "!join")
+        {
+            m_irc.SendMsg(response + "has joined the game");
+        }
+
+        if (messages[0].ToLower() == "!leave")
+        {
+            m_irc.SendMsg(response + "has left the game");
+        }
+        else
+        {
+            for (int i = 0; i < messages.Length; i++)
+            {
+
+            }
+        }
+
+        m_irc.SendMsg(response);
+    }
+}
