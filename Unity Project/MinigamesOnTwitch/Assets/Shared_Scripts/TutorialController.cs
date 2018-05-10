85cf57af63f3940d49f4457c8c16f4358cafdf5b
tommaddoxsoftware <32769550+tommaddoxsoftware@users.noreply.github.com>
2018-05-10 15:57:51 +0100

    Added more state checks
    Checks if animation clip is looping (Prompt user state e.g. "Press space to continue").
    Now triggers next state if space is pressed

--- Unity Project/MinigamesOnTwitch/Assets/Shared_Scripts/TutorialController.cs
+++ Unity Project/MinigamesOnTwitch/Assets/Shared_Scripts/TutorialController.cs
@@ -6,15 +6,29 @@ public class TutorialController : MonoBehaviour {
     [SerializeField]
     Animator anim;
 
+
     private void Start()
     {
         PlayerPrefs.SetInt("FirstTime", 1);
         anim.SetBool("FirstRun", true);
+
     }
 
     // Update is called once per frame
     void Update () {
-       
+        AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
+
+        if (state.loop)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                AnimTrigger();
+            }
+            //Visual Debug
+            //anim.SetBool("Loop state", true);
+        }
+        //else
+           // anim.SetBool("Loop state", false);
             
 	}
 
@@ -24,6 +38,7 @@ public class TutorialController : MonoBehaviour {
         {
             anim.SetBool("FirstRun", false);
         }
+
         anim.SetTrigger("Continue");
     }
 }
