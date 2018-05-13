d166566a297401817b4321dac423f0f57ad781bb
ElectricPie <32139670+ElectricPie@users.noreply.github.com>
2018-05-13 23:58:55 +0100

    Added Script, Areas For Commands and Kick Command
    Created the base layout for the script and set up the outline for the commands.
    Added kick functionally which checks ifs the player is currently playing and if they are it removes them from the player list.

---
+++ Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/Admin/MinigolfAdmin.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AdminControls)), 
+    RequireComponent(typeof(MinigolfController)),
+    RequireComponent(typeof(GameJoin))]
+public class MinigolfAdmin : MonoBehaviour {
+    private MinigolfController m_minigolf;
+    private GameJoin m_players;
+    private TwitchIRC m_irc;
+
+	// Use this for initialization
+	void Start () {
+        m_minigolf = this.GetComponent<MinigolfController>();
+        m_players = this.GetComponent<GameJoin>();
+        m_irc = this.GetComponent<TwitchIRC>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Commands(string user, string[] msgArray)
+    {
+
+
+        switch (msgArray[0].ToLower())
+        {
+            case "!resetall":
+                break;
+            case "!reset":
+                break;
+            case "!nextcourse":
+                break;
+            case "!finishall":
+                break;
+            case "!finish":
+                break;
+            case "!kick":
+                int playerIndex = m_players.CheckIfPlayer(msgArray[1].ToLower());
+
+                if (playerIndex >= 0)
+                {
+                    m_players.RemovePlayer(playerIndex);
+
+                    m_irc.SendMsg(msgArray[1].ToLower() + " has been removed from the game");
+                }
+                else
+                {
+                    m_irc.SendMsg(msgArray[1].ToLower() + " is not in the current game");
+                }
+                break;
+        }
+    }
+
+    private void KickPlayer()
+    {
+
+    }
+}
