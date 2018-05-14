dabe412c3acd7f6428448942789be5ea00d72c45
ElectricPie <32139670+ElectricPie@users.noreply.github.com>
2018-05-14 12:15:31 +0100

    Added Finish <player> and FinishAll Commands + Comments
    Finish all causes all balls to have their balls finish.
    Finish <player> only cause the selected players ball to finish.

--- Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/Admin/MinigolfAdmin.cs
+++ Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/Admin/MinigolfAdmin.cs
@@ -10,6 +10,7 @@ public class MinigolfAdmin : MonoBehaviour
     private MinigolfController m_minigolf;
     private GameJoin m_players;
     private TwitchIRC m_irc;
+    private LevelController m_levels;
 
     // Use this for initialization
     void Start()
@@ -17,6 +18,7 @@ public class MinigolfAdmin : MonoBehaviour
         m_minigolf = this.GetComponent<MinigolfController>();
         m_players = this.GetComponent<GameJoin>();
         m_irc = this.GetComponent<TwitchIRC>();
+        m_levels = this.GetComponent<LevelController>();
     }
 
     // Update is called once per frame
@@ -27,8 +29,6 @@ public class MinigolfAdmin : MonoBehaviour
 
     public void Commands(string user, string[] msgArray)
     {
-
-
         switch (msgArray[0].ToLower())
         {
             case "!resetall":
@@ -38,16 +38,47 @@ public class MinigolfAdmin : MonoBehaviour
             case "!nextcourse":
                 break;
             case "!finishall":
+                //Gets all active playerds
+                 for (int i = 0; i < m_players.GetActivePlayers().Count; i++)
+                {
+                    //Force finishes all active players
+                    m_levels.FinishBall(m_players.GetActivePlayers()[i]);
+                }
                 break;
             case "!finish":
-                break;
-            case "!kick":
+                //Prevents single word sentances from breaking
                 if (msgArray.Length > 1)
                 {
+                    //Get the player
                     int playerIndex = m_players.CheckIfPlayer(msgArray[1].ToLower());
 
                     if (playerIndex >= 0)
                     {
+                        //Finish the players ball
+                        m_levels.FinishBall(playerIndex);
+
+                        m_irc.SendMsg(msgArray[1].ToLower() + " has forcefully finished");
+                    }
+                    else
+                    {
+                        m_irc.SendMsg(msgArray[1].ToLower() + " is not in the current game");
+                    }
+                }
+                else
+                {
+                    m_irc.SendMsg(user + " invalid user");
+                }
+                break;
+            case "!kick":
+                //Prevents single word sentances from breaking
+                if (msgArray.Length > 1)
+                {
+                    //Get the players indnex
+                    int playerIndex = m_players.CheckIfPlayer(msgArray[1].ToLower());
+
+                    if (playerIndex >= 0)
+                    {
+                        //Removes the player
                         m_players.RemovePlayer(playerIndex);
 
                         m_irc.SendMsg(msgArray[1].ToLower() + " has been removed from the game");
