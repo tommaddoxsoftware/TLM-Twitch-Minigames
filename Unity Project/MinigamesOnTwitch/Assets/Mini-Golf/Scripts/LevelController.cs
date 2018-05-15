819c4a1a390c6ae886e4ec92078e8f9c4623a291
Szymon <szymon.buczek@students.plymouth.ac.uk>
2018-05-15 12:19:04 +0100

    Edited Level Controller script for sound
    Edited the Level Controller script for the sounds: PlayerCompletedCourse and CourseCompleted.
    
    Removed the unnecessary scripting for playing the sounds based on how many players are in the scene, etc.
    Added a function that plays the CourseCompleted sound after 0.5 seconds.
    This function is used when all players have completed the current course

--- Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/LevelController.cs
+++ Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/LevelController.cs
@@ -89,16 +89,19 @@ public class LevelController : MonoBehaviour
         //Finds the index of the ball in the ball array
         int ballIndex = m_ballControl.FindBallIndex(ball);
 
+        // Play sound PlayerCompleteCourse1 when a player collides with the hole
+        FindObjectOfType<AudioManager>().Play("PlayerCompletedCourse");
+
         // Finds Objects with the tag "Player", stores them in an array activePlayersInScene
-        GameObject[] activePlayersInScene = GameObject.FindGameObjectsWithTag("Player");
-        if (activePlayersInScene.Length > 1) // if the amount of active players is more than 1
-        {
-            FindObjectOfType<AudioManager>().Play("PlayerCompleteCourse1"); // play sound PlayerCompleteCourse1
-        }
-        else if (activePlayersInScene.Length <= 1) // if the amount of active players is less than or equal to 1
-        {
-            FindObjectOfType<AudioManager>().Play("PlayerCompleteCourse3"); // play sound PlayerCompleteCourse3
-        }
+        //GameObject[] activePlayersInScene = GameObject.FindGameObjectsWithTag("Player");
+        //if (activePlayersInScene.Length > 1) // if the amount of active players is more than 1
+        //{
+        //    FindObjectOfType<AudioManager>().Play("PlayerCompleteCourse1"); // play sound PlayerCompleteCourse1
+        //}
+        //else if (activePlayersInScene.Length <= 1) // if the amount of active players is less than or equal to 1
+        //{
+        //    FindObjectOfType<AudioManager>().Play("PlayerCompleteCourse3"); // play sound PlayerCompleteCourse3
+        //}
 
         //If the object isnt a ball do nothing
         if (ballIndex != -1)
@@ -142,12 +145,18 @@ public class LevelController : MonoBehaviour
                 //Move the balls to the new start point
                 m_ballControl.MoveBalls();
 
-                // Play sound if all players have completed the current course
-                FindObjectOfType<AudioManager>().Play("PlayerCompleteCourse3");
+                // Run PlayCourseCompleted after 0.5 seconds
+                Invoke("PlayCourseCompleted", 0.5f);
 
                 ResetBallState();
             }
         }
+    }
+
+    // Play sound for all players completed the current course
+    private void PlayCourseCompleted()
+    {
+        FindObjectOfType<AudioManager>().Play("CourseCompleted"); // find AudioManager in scene and play sound
     }
 
     //Resets the players states to false
