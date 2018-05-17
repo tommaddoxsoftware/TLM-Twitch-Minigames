587898e2b5fc19786f32135a240581353f12485a
tommaddoxsoftware <32769550+tommaddoxsoftware@users.noreply.github.com>
2018-05-17 21:58:06 +0100

    Updated Scoreboard functionality
    Now updates the scoreboard properly, and adds players to the leaderboard. Added a timeout on leaderboard (closes after 5 seconds)

--- Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/Ui/UiController.cs
+++ Unity Project/MinigamesOnTwitch/Assets/Mini-Golf/Scripts/Ui/UiController.cs
@@ -33,6 +33,8 @@ public class UiController : MonoBehaviour {
     //Entire Game UI panel
     [SerializeField]
     private GameObject gameUi;
+    [SerializeField]
+    private GameObject leaderboard;
 
     //Stores the total number of courses
     private int numCourses;
@@ -200,6 +202,29 @@ public class UiController : MonoBehaviour {
 
         List<Score> playerScores = new List<Score>();
 
+
+        //Remove all current items in scoreboard name colum
+        foreach(Transform child in nameContainer.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        //Remove all current items in scoreboard scores
+        int currHole = playerList.GetComponent<LevelController>().CurrentCourse;
+        for (int k = 0; k <= currHole; k++)
+        {
+            foreach (Transform child in holeContainers[k].transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+
+        //Remove all items in overall column
+        foreach(Transform child in holeContainers[9].transform)
+        {
+            Destroy(child.gameObject);
+        }
+
         for(int i=0; i<currPlayers.Count; i++)
         {
 
@@ -215,6 +240,35 @@ public class UiController : MonoBehaviour {
         //Sort each player by score
         List<Score> sortedList = new List<Score>();
         sortedList = playerScores.OrderBy(x => x.playerScore).ToList();
+
+        for(int j=0; j<sortedList.Count; j++)
+        {
+            //Instantiate each player's text object on the scoreboard
+            GameObject name = Instantiate(usrNamePrefab, nameContainer.transform);
+            name.GetComponent<Text>().text = sortedList[j].playerName;
+
+            //Add each player's score per hole            
+            for(int holeIndex = 0;  holeIndex<=currHole; holeIndex++)
+            {
+                GameObject score = Instantiate(usrScorePrefab, holeContainers[holeIndex].transform);
+                int courseScore = playerList.GetComponent<MinigolfController>().m_playerBalls[sortedList[j].ballID].GetComponent<Ball>().playerScore[holeIndex];
+                score.GetComponent<Text>().text = courseScore.ToString();
+            }
+
+            //Add Overall score            
+            GameObject Overall = Instantiate(usrScorePrefab, holeContainers[9].transform);
+            Overall.GetComponent<Text>().text = sortedList[j].playerScore.ToString();
+        }
+        leaderboard.SetActive(true);
+
+        StartCoroutine(CloseLeaderboard());
+        
+    }
+
+    private IEnumerator CloseLeaderboard()
+    {
+        yield return new WaitForSeconds(5);
+        leaderboard.SetActive(false);
     }
 
     public void FadeIn(float fadeSpeed, Text textToUse)
